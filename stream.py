cbb6f52db5adfad7d005abb6a229c5546f067228
eat-sleep-code <info@eat-sleep-code.com>
2021-03-29 23:04:23 -0700

    Change background color and handle favicon for Chrome.

--- stream.py
+++ stream.py
@@ -1,8 +1,8 @@
 import io
 import picamera
 import logging
-import socket
 import socketserver
+import subprocess
 from threading import Condition
 from http import server
 
@@ -10,87 +10,91 @@ PAGE="""\
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Camera Zero</title>
-    <meta charset="utf-8">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-    <meta name="application-name" content="Camera Zero">
-    <meta name="theme-color" content="#000000">
+	<title>Camera Zero</title>
+	<meta charset="utf-8">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+	<meta name="application-name" content="Camera Zero">
+	<meta name="theme-color" content="#000000">
 </head>
-<body>
-    <div style="display: flex; align-items: center; width: 100vw; justify-content: center; height: 100vh;">
-        <img src="stream.mjpg" style="width: 640px;" />
-    </div>
+<body style="margin: 0; padding: 0;">
+	<div style="background: rgba(8, 8, 8, 1.0); display: flex; align-items: center; width: 100vw; justify-content: center; height: 100vh;">
+		<img src="stream.mjpg" style="width: 100%; max-width: 640px;" />
+	</div>
 </body>
 </html>
 """
 
 class StreamingOutput(object):
-    def __init__(self):
-        self.frame = None
-        self.buffer = io.BytesIO()
-        self.condition = Condition()
+	def __init__(self):
+		self.frame = None
+		self.buffer = io.BytesIO()
+		self.condition = Condition()
 
-    def write(self, streamBuffer):
-        if streamBuffer.startswith(b'\xff\xd8'):
-            self.buffer.truncate()
-            with self.condition:
-                self.frame = self.buffer.getvalue()
-                self.condition.notify_all()
-            self.buffer.seek(0)
-        return self.buffer.write(streamBuffer)
+	def write(self, streamBuffer):
+		if streamBuffer.startswith(b'\xff\xd8'):
+			self.buffer.truncate()
+			with self.condition:
+				self.frame = self.buffer.getvalue()
+				self.condition.notify_all()
+			self.buffer.seek(0)
+		return self.buffer.write(streamBuffer)
 
 
 class StreamingHandler(server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            content = PAGE.encode('utf-8')
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(content))
-            self.end_headers()
-            self.wfile.write(content)
-        elif self.path == '/stream.mjpg':
-            self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-            self.end_headers()
-            try:
-                while True:
-                    with output.condition:
-                        output.condition.wait()
-                        frame = output.frame
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
-                    self.end_headers()
-                    self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
-            except Exception as e:
-                logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
-        else:
-            self.send_error(404)
-            self.end_headers()
+	def do_GET(self):
+		if self.path == '/':
+			content = PAGE.encode('utf-8')
+			self.send_response(200)
+			self.send_header('Content-Type', 'text/html')
+			self.send_header('Content-Length', len(content))
+			self.end_headers()
+			self.wfile.write(content)
+		elif self.path == '/stream.mjpg':
+			self.send_response(200)
+			self.send_header('Age', 0)
+			self.send_header('Cache-Control', 'no-cache, private')
+			self.send_header('Pragma', 'no-cache')
+			self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+			self.end_headers()
+			try:
+				while True:
+					with output.condition:
+						output.condition.wait()
+						frame = output.frame
+					self.wfile.write(b'--FRAME\r\n')
+					self.send_header('Content-Type', 'image/jpeg')
+					self.send_header('Content-Length', len(frame))
+					self.end_headers()
+					self.wfile.write(frame)
+					self.wfile.write(b'\r\n')
+			except Exception as e:
+				logging.warning(
+					'Removed streaming client %s: %s',
+					self.client_address, str(e))
+		elif self.path == '/favicon.ico':
+			self.send_response(200)
+			self.send_header('Content-Type', 'image/x-icon')
+			self.send_header('Content-Length', 0)
+			self.end_headers()
+		else:
+			self.send_error(404)
+			self.end_headers()
 
 class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
-    allow_reuse_address = True
-    daemon_threads = True
+	allow_reuse_address = True
+	daemon_threads = True
 
-# 4 fps = 6 Mbps
-#24 fps = 18 Mbps
 with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
-    output = StreamingOutput()
-    camera.start_recording(output, format='mjpeg')
-    try:
-        address = ('', 80)
-        server = StreamingServer(address, StreamingHandler)
-        server.serve_forever()
-        hostname = socket.gethostbyname(socket.gethostname())
-        print('Stream started: http://' + str(hostname))
-    finally:
-        camera.stop_recording()
-        print('Stream ended')
+	output = StreamingOutput()
+	camera.start_recording(output, format='mjpeg')
+	hostname = subprocess.getoutput('hostname -I')
+	url = 'http://' + str(hostname)
+	print('\n Stream started: ' + url + '\n')
+	try:
+		address = ('', 80)
+		server = StreamingServer(address, StreamingHandler)
+		server.serve_forever()
+	finally:
+		camera.stop_recording()
+		print('\n Stream ended \n')
